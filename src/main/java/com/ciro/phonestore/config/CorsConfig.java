e05f6d28f7498e5c9dc0850e8ac4f51a483484de
OminduKumara <IT23575776@my.sliit.lk>
2025-05-21 15:40:02 +0530

    added mysql code

--- src/main/java/com/ciro/phonestore/config/CorsConfig.java
+++ src/main/java/com/ciro/phonestore/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.ciro.phonestore.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,11 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
+    public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://mobilephoneshop.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("*");
             }
